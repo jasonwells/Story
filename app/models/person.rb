81e53b66ef4c7565a6020e841fd729099c51fb3a
Jason Wells <flipstock@gmail.com>
2012-03-18 17:57:52 -0600

    Added sibling query for Person
    The sibling query allows for the person view to enumerate all brothers
    and sisters.

--- app/models/person.rb
+++ app/models/person.rb
@@ -4,6 +4,7 @@ class Person < ActiveRecord::Base
   default_scope :order => 'birth ASC'
   
   has_many :children, :class_name => "Person"
+  has_many :siblings, :class_name => "Person"
   has_many :events
   has_many :photos
   belongs_to :father, :class_name => "Person", :foreign_key => 'father_id'
@@ -17,6 +18,10 @@ class Person < ActiveRecord::Base
   
   def children
     Person.find(:all, :conditions => ['father_id=? or mother_id=?', id, id])
+  end
+  
+  def siblings
+    Person.find(:all, :conditions => ['(father_id=? or mother_id=?) and id!=?', father_id, mother_id, id])
   end
   
   def photos
